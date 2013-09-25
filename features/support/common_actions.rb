74ae0e87e3924744bd7b284b585a3137395a9428
chandrak <chandrak@blinkbox.com>
2013-09-25 15:56:37 +0100

    updating utilities and common actions

--- features/support/common_actions.rb
+++ features/support/common_actions.rb
@@ -1,17 +1,25 @@
+module Discover
   def search_blinkbox_books(search_word)
     fill_in('term', :with => "#{search_word}")
     click_button('submit_desk')
   end
 
-  def fill_form_element (element, value)
-    fill_in("#{element}", :with => "#{value}")
+  def click_book_details
+    within('[class="expandable itemsets"]') do
+      within(first('.book')) do
+        @book_href= find('[data-test="book-title-cover"]')[:href]
+        find('[data-test="book-title-cover"]').click
+      end
+    end
   end
 
-  def click_page_link (page_element)
-    find("[data-test='#{page_element}']").click
-  end
+end
+
 
 module SignInAndRegistration
+  def click_register_button
+    find('[class="blue_button ng-binding"]').click
+  end
 
   def enter_personal_details
     email_address = generate_random_email_address
@@ -26,12 +34,115 @@ module SignInAndRegistration
     fill_form_element('repassword', value)
   end
 
-  def enter_valid_sign_in_details (email_address,password)
+  def enter_valid_sign_in_details (email_address, password)
     fill_form_element('email', email_address)
     fill_form_element('password', password)
   end
+
+  def click_sign_in_button
+    find('[class="yellow_button float-right ng-binding"]').click
+  rescue
+    find("[data-test='#{get_element_id_for("Sign in")}']").click
+
+  end
+
+  def accept_terms_and_conditions
+    check('termsconditions')
+  end
+
+  def submit_registration_details
+    find('[class="yellow_button ng-binding"]').click
+  end
+
+  def register_new_user
+    enter_personal_details
+    choose_a_valid_password('test1234')
+    accept_terms_and_conditions
+    submit_registration_details
+  end
+
+  def sign_in(email_address, password)
+    click_sign_in_button
+    enter_valid_sign_in_details(email_address, password)
+    click_sign_in_button
+  end
 end
+
+
+module BuyABook
+  def enter_card_number(number)
+    fill_form_element('number_card', number)
+  end
+
+  def select_expiry_date (month, year)
+    select_value('card_dates_month', month)
+    select_value('card_dates_year', year)
+  end
+
+  def enter_name_on_card(name)
+    fill_form_element('card_name', name)
+  end
+
+  def enter_address_line_one(line_one)
+    fill_form_element('address_one', line_one)
+  end
+
+  def enter_address_line_two(line_two)
+    fill_form_element('address_two', line_two)
+  end
+
+  def enter_town_or_city(town_or_city)
+    fill_form_element('address_three', town_or_city)
+  end
+
+  def enter_post_code(post_code)
+    fill_form_element('address_four', post_code)
+  end
+
+
+  def enter_new_payment_details(card_type)
+    enter_card_number(get_card_number_by_type(card_type))
+    select_expiry_date('12', '2023')
+    enter_name_on_card('jamie jones')
+  end
+
+  def enter_billing_details
+    enter_address_line_one('my address line one')
+    enter_address_line_two('my address line two')
+    enter_town_or_city('My town')
+    enter_post_code('WC18AQ')
+  end
+
+  def click_confirm_and_pay
+    find('[class="yellow_button ng-binding"]')
+  end
+
+  def save_card_details()
+    check('save_details')
+  end
+
+  def click_buy_now_in_book_details
+    find('[class="details"]').should be_visible
+    begin
+      sleep(2)
+    end until (find('[data-test="book-buy-button"]', :visible => true).visible?)
+    click_button('BUY NOW')
+  end
+end
+
+
+module ManageMyAccount
+
+  def click_link_from_my_account_dropdown(link)
+    page.execute_script("document.getElementById('options').getElementsByTagName('ul')[0].style.display='inline-block'")
+    find("[title='#{link}']").click
+    page.execute_script("document.getElementById('options').getElementsByTagName('ul')[0].style.display='none'")
+  end
+end
+World(Discover)
 World(SignInAndRegistration)
+World(BuyABook)
+World(ManageMyAccount)
 
 
 
--- features/support/utilities.rb
+++ features/support/utilities.rb
@@ -1,34 +1,47 @@
 module WebUtilities
 
-def find_a_text selector,type
-  within(selector) do
-    case type
-      when 'author'
-        @actual_text = find('[data-test="book-authors"]')[:title]
+  def find_a_text selector, type
+    within(selector) do
+      case type
+        when 'author'
+          @actual_text = find('[data-test="book-authors"]')[:title]
 
-      when 'title'
+        when 'title'
           @actual_text= find('[data-test="book-title"]')[:title]
+      end
     end
+    return @actual_text
   end
-  return @actual_text
+
+  def generate_random_email_address
+    first_part = 'cucumber_test'
+    last_part = '@blinkboxbooks.com'
+    middle_part = rand(1..9999).to_s
+    return first_part+middle_part+last_part
+  end
+
+  def generate_random_first_name
+    first_part = 'autotest-'
+    last_part=(0...10).map { ('a'..'z').to_a[rand(26)] }.join
+    return first_part + last_part
+  end
+
+  def delete_cookies
+    Capybara.current_session.reset_session!
+  end
+
+  def fill_form_element (element, value)
+    fill_in("#{element}", :with => "#{value}")
+  end
+
+  def click_link_or_button (element_id)
+    find("[data-test='#{element_id}']").click
+  end
+
+  def select_value(element, value)
+    select(value, :from => element)
+  end
 end
 
-def generate_random_email_address
-  first_part = 'cucumber_test'
-  last_part = '@blinkboxbooks.com'
-  middle_part = rand(1..9999).to_s
-  return first_part+middle_part+last_part
-end
-
-def generate_random_first_name
-  first_part = 'autotest-'
-  last_part=(0...10).map{ ('a'..'z').to_a[rand(26)] }.join
-  return  first_part + last_part
-end
-
-def delete_cookies
-  Capybara.current_session.reset_session!
-end
-end
 
 World(WebUtilities)
