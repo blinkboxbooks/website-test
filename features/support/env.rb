14d10aa1b025313ef54dc019850dda804060e29c
aliaksandr <aliaksandr@blinkbox.com>
2013-10-16 23:47:45 +0100

    fix htmlunit config

--- features/support/env.rb
+++ features/support/env.rb
@@ -52,7 +52,7 @@ if TEST_CONFIG['GRID'] =~ /^true|on$/i
   caps = case TEST_CONFIG['BROWSER_NAME'].downcase
     when 'firefox', 'safari', 'ie', 'chrome'
       Selenium::WebDriver::Remote::Capabilities.send(TEST_CONFIG['BROWSER_NAME'].downcase.to_sym)
-    when 'HTMLUNIT'
+    when 'htmlunit'
       Selenium::WebDriver::Remote::Capabilities.htmlunit(:javascript_enabled => true)
     else
       raise "Not supported browser: #{TEST_CONFIG['BROWSER_NAME']}"
