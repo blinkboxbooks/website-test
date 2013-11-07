530a103efe6551b9d948bfe7d2760fdfa7f95697
aliaksandr <aliaksandr@blinkbox.com>
2013-11-07 00:06:19 +0000

    revert urls for terms-conditions and free-books pages as those are not deployed to production yet

--- features/support/page_models/pages/free_books_page.rb
+++ features/support/page_models/pages/free_books_page.rb
@@ -1,7 +1,7 @@
 module PageModels
   class FreeBooksPage < PageModels::BlinkboxbooksPage
     set_url "/#!/free-books"
-    set_url_matcher /free-books/
+    set_url_matcher /free_books/
   end
 
   register_model_caller_method(FreeBooksPage)
