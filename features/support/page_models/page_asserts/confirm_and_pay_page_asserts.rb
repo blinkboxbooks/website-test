afb2f13baf6a61d85b3800147d57a22f928fd674
chandrak <chandrak@blinkbox.com>
2013-12-13 15:36:13 +0000

    updated as per comments on pull reqeust

--- features/support/page_models/page_asserts/confirm_and_pay_page_asserts.rb
+++ features/support/page_models/page_asserts/confirm_and_pay_page_asserts.rb
@@ -1,15 +1,14 @@
 module PageModels
   module ConfirmAndPayPageAsserts
       def assert_confirm_and_pay_button_status(button_status)
-
+        selector = confirm_and_pay_page.confirm_and_pay
         if (button_status.include?('disabled'))
-          find_button('Confirm & pay', disabled: true)[:disabled].eql?('true')
+          find_button(selector.text, disabled: true)[:disabled].eql?('true')
         else
-          find_button('Confirm & pay')[:disabled].should_not be
+          find_button(selector.text)[:disabled].should_not be
         end
       rescue Capybara::ElementNotFound
         raise "Confirm and Pay button is not #{button_status} as expected"
-
       end
     end
 end
