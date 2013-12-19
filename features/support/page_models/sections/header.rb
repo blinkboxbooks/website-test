4db9f1ca073f7756d7c0e6ac37a3ab40ae314412
chandrak <chandrak@blinkbox.com>
2013-12-19 17:09:30 +0000

    fixing smoke tests and adding support code to new user cancelling order

--- features/support/page_models/sections/header.rb
+++ features/support/page_models/sections/header.rb
@@ -4,14 +4,16 @@ module PageModels
     element :main_pages_navigation, "div#main-navigation"
 
     def account_nav_link(link_name)
-      title = link_name.gsub("&", "&amp;")
-      account_options_dropdown.find("a[title=\"#{title}\"]")
+      #title = link_name.gsub("&", "&amp;")
+      #account_options_dropdown.find("a[title=\"#{title}\"]")
+      account_options_dropdown.find("a", :text => "#{link_name}")
     end
 
     def navigate_to_account_option(link_name)
       self.should have_account_options_dropdown
       account_options_dropdown.click
-      click_link(link_name)
+      account_nav_link(link_name).click
+      #click_link(link_name)
     end
 
     def main_page_navigation(page_name)
