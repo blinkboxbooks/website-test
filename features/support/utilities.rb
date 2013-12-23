be2709848ce2da772ddf6f32a362885e85f9c5ac
chandrak <chandrak@blinkbox.com>
2013-12-23 14:59:31 +0000

    Fixing regression tests

--- features/support/utilities.rb
+++ features/support/utilities.rb
@@ -114,7 +114,7 @@ module BlinkboxWebUtilities
   end
 
   def assert_support_page(page_name)
-    assert page.driver.browser.window_handles.size == 2
+    page.driver.browser.window_handles.size.should be == 2
     new_window = page.driver.browser.window_handles.last
     page.within_window new_window do
       current_url.should match Regexp.new(get_support_page_url(page_name))
