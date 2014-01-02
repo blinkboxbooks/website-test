122d2c46fa353c8405c3d1364ea905fcfb607b11
chandrak <chandrak@blinkbox.com>
2014-01-02 13:11:54 +0000

    updating bestsellers page class name as per the page name change

--- features/support/page_models/pages/best_sellers_page.rb
+++
@@ -1,8 +0,0 @@
-module PageModels
-  class BestSellersPage < PageModels::BlinkboxbooksPage
-    set_url "/#!/bestsellers"
-    set_url_matcher /bestsellers/
-  end
-
-  register_model_caller_method(BestSellersPage)
-end
