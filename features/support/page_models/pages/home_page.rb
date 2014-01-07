fe3dd3bd66c842c9b23061c0adb4557b3417b9f1
chandrak <chandrak@blinkbox.com>
2014-01-07 16:20:05 +0000

    Removig absolete tests from regression suite

--- features/support/page_models/pages/home_page.rb
+++ features/support/page_models/pages/home_page.rb
@@ -3,6 +3,7 @@ module PageModels
     set_url "/"
     set_url_matcher Regexp.new("#{Capybara.app_host}\/?(?:#!/)?$")
     element :book_in_the_news, '#books_news'
+    element :editors_picks, '#editors_picks'
   end
 
   register_model_caller_method(HomePage)
