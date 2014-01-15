32c980dbd82a38266434ecc4e73c1ba6a869343d
chandrak <chandrak@blinkbox.com>
2014-01-15 15:43:46 +0000

    Adding journe scenarios and regression fixes

--- features/step_definitions/manage_steps.rb
+++ features/step_definitions/manage_steps.rb
@@ -30,6 +30,8 @@ When /^I edit marketing preferences$/ do
 end
 
 And /^marketing preferences are as submitted$/  do
+  click_link('Featured')
+  click_link_from_my_account_dropdown('Your personal details')
   assert_marketing_preferences_changed(@after_status)
 end
 
--- features/support/page_models/pages/reset_password_page.rb
+++ features/support/page_models/pages/reset_password_page.rb
@@ -4,7 +4,7 @@ module PageModels
     set_url_matcher /reset-password/
     element :email_address, '#email'
     element :not_remember_email_id, "a", :text => "I can't remember which email I used"
-    element :send_reset_link,"button", :text => "Send reset link"
+    element :send_reset_link,"button", :text => "SEND RESET LINK"
   end
 
   register_model_caller_method(ResetPasswordPage)
