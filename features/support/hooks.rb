a788f0da4fff888f3a133260c2e9957a529e9008
chandrak <chandrak@blinkbox.com>
2014-02-07 15:39:06 +0000

    adding signout for after hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,14 +4,20 @@ Before("~@reset_session") do
   puts "Web App Version: #{app_version_info}"
 end
 
-if TEST_CONFIG["fail_fast"]
-  puts "'FAIL FAST' option is ON"
   After do |scenario|
+    if TEST_CONFIG["fail_fast"]
+      puts "'FAIL FAST' option is ON"
+
     # Tell Cucumber to quit after this scenario is done - if it failed
     if scenario.failed?
       puts "Terminating the run after the first failure for quicker feedback.\n" +
                "See above for the actual failure, or check the HTML report."
       Cucumber.wants_to_quit = true
     end
+    end
+     if(current_page.header.welcome.has_text?("Hi"))
+      click_link_from_my_account_dropdown('Sign out')
+      raise "User is already signed out, continue with test execution"
+     end
+
   end
-end
