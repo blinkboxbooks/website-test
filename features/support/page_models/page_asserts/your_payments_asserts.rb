8c8b62195c6c03806ec8e2521b86d1d2c7c89d5e
gabor <gabor@blinkbox.com>
2014-05-23 12:52:52 +0100

    features/manage/manage_your_payments.feature

--- features/support/page_models/page_asserts/your_payments_asserts.rb
+++ features/support/page_models/page_asserts/your_payments_asserts.rb
@@ -16,11 +16,8 @@ module PageModels
 
     def assert_payment_card_saved(card_count, name_on_card, card_type)
       your_account_page.wait_until_spinner_invisible
-      within(your_payments_page.saved_cards_container) do
-        your_payments_page.should have_saved_cards :count => card_count
-        page.text.downcase.should include name_on_card.downcase
-        page.text.downcase.should include card_type.downcase
-      end
+      expect(your_payments_page).to have_saved_cards(:count => card_count)
+      expect(your_payments_page.saved_cards).to have_credit_card(:holder_name => name_on_card.downcase, :type => card_type.downcase)
     end
   end
 end
