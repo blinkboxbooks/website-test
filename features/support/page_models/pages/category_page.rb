370153cdd3681e6eca576b02b8073fb7398b58c6
dthakkar <dhaval@blinkbox.com>
2014-05-30 17:34:54 +0100

    Shifting category URL to Category page model as per Alex's comment on PR406

--- features/support/page_models/pages/category_page.rb
+++ features/support/page_models/pages/category_page.rb
@@ -7,5 +7,10 @@ module PageModels
     element :category_name, '[data-test="category-title"]'
   end
 
+  def show?(category_name)
+    category_url = category_name.downcase.gsub(/&|'|and|,/, '').gsub(/ +/, '-')
+    current_url.include?(category_url)
+  end
+
   register_model_caller_method(CategoryPage)
 end
