3170ff61604f6c21176c2d56e35d62d9969effd5
Alex <alexa@blinkbox.com>
2014-06-03 14:56:14 +0100

    Refactor common bits.

--- teamcity-run.sh
+++ teamcity-run.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 # Run tests within TeamCity and rerun on failure.
+function error { 
+	echo "$@" 1>&2; 
+}
+
+function run_tests_outputting_to {
+	bundle exec cucumber -p ci-smoke-local --tags @production HEADLESS=true FAIL_FAST=false -f rerun --out "$1" || true
+}
+
 
 # Ignore output - thus making this return true regardless of real error code.
-bundle exec cucumber -p ci-smoke-local --tags @production HEADLESS=true FAIL_FAST=false -f rerun --out ./rerun.txt || true
+run_tests_outputting_to ./rerun.txt
 
 if [ -e './rerun.txt' ]; then
 	
-	echo "*********************************"
-	echo "TESTS FAILED."
-	echo "Rerunning tests."
-	echo "*********************************"
+	error "*********************************"
+	error "TESTS FAILED."
+	error "Rerunning tests."
+	error "*********************************"
 
 	for i in {1..3}; do
 		response=1
-		
+
 		echo "Rerun attempt $i/3"
 
 		# Only rerun the first set of failures.
-		bundle exec cucumber -p ci-smoke-local @rerun.txt HEADLESS=on FAIL_FAST=false  -f rerun --out "./rerun-$i.txt" || true
+		run_tests_outputting_to "./rerun-$i.txt"
 
 		# If rerun doesn't exist...
 		if [ ! -f "./rerun-$i.txt" ]; then
@@ -26,6 +34,5 @@ if [ -e './rerun.txt' ]; then
 		fi
 	done
 
-	echo $response
 	exit $response
 fi
