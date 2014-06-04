df5a4af8e0a694ac4bd79a2b5fe1ca561c1d24a5
dthakkar <dhaval@blinkbox.com>
2014-06-04 16:07:29 +0100

    Updated page models for elements to be defined by data-test tags -  TTWG-77.

--- features/support/page_models/pages/reset_password_page.rb
+++ features/support/page_models/pages/reset_password_page.rb
@@ -3,8 +3,8 @@ module PageModels
     set_url "#!/reset-password"
     set_url_matcher /reset-password/
     element :email_address, '#email'
-    element :not_remember_email_id, "a", :text => /I can't remember which email I used/i
-    element :send_reset_link,"button", :text => /Send Reset Link/i
+    element :not_remember_email_id, 'a[data-test="cant-remember-email-link"]'
+    element :send_reset_link, 'button[data-test="send-reset-link-button"]'
   end
 
   register_model_caller_method(ResetPasswordPage)
