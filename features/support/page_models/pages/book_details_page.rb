3a131f42a4ed4b1cdaa6f5753caa122669985d30
gabor <gabor@blinkbox.com>
2014-08-08 13:22:03 +0100

    Review changes

--- features/support/page_models/pages/book_details_page.rb
+++ features/support/page_models/pages/book_details_page.rb
@@ -6,7 +6,7 @@ module PageModels
     element :read_offline, ".read-offline"
     element :buy_now, 'button[data-test="book-buy-button"]'
     iframe :reader, Reader, '#cpr-iframe'
-    element :title_element, 'h2.title', :match => :first
+    element :title_element, '#book-details h2.title', :match => :first
 
     def title
       wait_for_title_element
@@ -18,8 +18,8 @@ module PageModels
       case book_type
         when :free then self.load(:isbn => test_data('library_isbns', 'free_book'), :title => 'test-book')
         when :paid then self.load(:isbn => test_data('library_isbns', 'pay_for_book'), :title => 'test-book')
-        when :free_sample then self.load(:isbn => test_data('library_isbns', 'free_sample'), :title => 'test-book')
-        when :paid_sample then self.load(:isbn => test_data('library_isbns', 'pay_for_sample'), :title => 'test-book')
+        when :free_sample then self.load(:isbn => test_data('library_isbns', 'sample_for_free_book'), :title => 'test-book')
+        when :paid_sample then self.load(:isbn => test_data('library_isbns', 'sample_for_paid_book'), :title => 'test-book')
         else raise "Cannot load book details page with unknown book type: #{book_type.to_s}"
       end
     end
