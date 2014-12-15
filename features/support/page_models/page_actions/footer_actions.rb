f506d2b222be42fbcf4c09e0c1e5a25a89954528
gabor <gabor@blinkbox.com>
2014-12-15 10:01:32 +0000

    Correct format in footer actions

--- features/support/page_models/page_actions/footer_actions.rb
+++ features/support/page_models/page_actions/footer_actions.rb
@@ -1,13 +1,21 @@
-def click_author_link_on_footer(author_name)
-  link = current_page.footer.author_by_name(author_name)
-  !link.nil? ? link.click : raise("Unable to find an author by name #{author_name}")
+module PageModels
+  module FooterActions
+
+    def click_author_link_on_footer(author_name)
+      link = current_page.footer.author_by_name(author_name)
+      !link.nil? ? link.click : raise("Unable to find an author by name #{author_name}")
+    end
+
+    def click_category_link_on_footer(category_name)
+      link = current_page.footer.category_by_name(category_name)
+      !link.nil? ? link.click : raise("Unable to find category by name #{category_name}")
+    end
+    
+    def click_footer_link(link_name)
+      current_page.footer.navigate_by_link(link_name)
+    end
+
+  end
 end
 
-def click_category_link_on_footer(category_name)
-  link = current_page.footer.category_by_name(category_name)
-  !link.nil? ? link.click : raise("Unable to find category by name #{category_name}")
-end
-
-def click_footer_link(link_name)
-  current_page.footer.navigate_by_link link_name
-end
+World(PageModels::FooterActions)
