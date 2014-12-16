6310602f92b8a755bc52b2984b90b648caab2b0a
gabor <gabor@blinkbox.com>
2014-12-16 13:12:26 +0000

    Addressing review comment

--- features/support/page_models/page_actions/footer_actions.rb
+++ features/support/page_models/page_actions/footer_actions.rb
@@ -3,12 +3,14 @@ module PageModels
 
     def click_author_link_on_footer(author_name)
       link = current_page.footer.author_by_name(author_name)
-      !link.nil? ? link.click : raise("Unable to find an author by name #{author_name}")
+      fail("Unable to find an author by name #{author_name}") if link.nil?
+      link.click
     end
 
     def click_category_link_on_footer(category_name)
       link = current_page.footer.category_by_name(category_name)
-      !link.nil? ? link.click : raise("Unable to find category by name #{category_name}")
+      fail("Unable to find category by name #{category_name}") if link.nil?
+      link.click
     end
 
     def click_footer_link(link_name)
