b5a886c69df5c979ef65f9a1ca32229da22d99d8
gabor <gabor@blinkbox.com>
2015-01-06 13:56:51 +0000

    Sync optimisation for pagination section

--- features/support/page_models/sections/pagination.rb
+++ features/support/page_models/sections/pagination.rb
@@ -5,27 +5,30 @@ module PageModels
     element :previous_button, '[data-test="pagination-previous"]'
     element :next_button, '[data-test="pagination-next"]'
 
-    def click_on_previous_button
-      wait_until_pagination_numbers_visible
-      previous_button.click
+    def wait_for_numbers
+      wait_for_pagination_numbers
       wait_until_pagination_numbers_visible
     end
 
+    def click_on_previous_button
+      wait_for_numbers
+      previous_button.click
+    end
+
     def click_on_next_button
-      wait_until_pagination_numbers_visible
+      wait_for_numbers
       next_button.click
     end
 
     def click_on_pagination_number(page_number)
-      wait_until_pagination_numbers_visible
+      wait_for_numbers
       pagination_numbers[page_number.to_i-1].find('a').click
     end
 
     def selected_page_number
-      wait_until_pagination_numbers_visible
+      wait_for_numbers
       pagination_numbers.find { |page| page[:class].include?('active') }
     end
 
   end
 end
-
