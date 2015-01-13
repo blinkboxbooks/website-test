de1ed09a11bff790bca91e39e2ae8e913f314622
gabor <gabor@blinkbox.com>
2015-01-13 17:37:15 +0000

    More descriptive method name

--- features/support/page_models/sections/pagination.rb
+++ features/support/page_models/sections/pagination.rb
@@ -5,28 +5,28 @@ module PageModels
     element :previous_button, '[data-test="pagination-previous"]'
     element :next_button, '[data-test="pagination-next"]'
 
-    def wait_for_numbers
+    def wait_for_pagination_controls
       wait_for_pagination_numbers
       wait_until_pagination_numbers_visible
     end
 
     def click_on_previous_button
-      wait_for_numbers
+      wait_for_pagination_controls
       previous_button.click
     end
 
     def click_on_next_button
-      wait_for_numbers
+      wait_for_pagination_controls
       next_button.click
     end
 
     def click_on_pagination_number(page_number)
-      wait_for_numbers
+      wait_for_pagination_controls
       pagination_numbers[page_number.to_i-1].find('a').click
     end
 
     def selected_page_number
-      wait_for_numbers
+      wait_for_pagination_controls
       pagination_numbers.find { |page| page[:class].include?('active') }
     end
 
